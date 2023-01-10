9000c911b9b883c75b352beb8818dbcc00e361b7
rati <ratiruxadzee@gmail.com>
2023-01-10 19:31:18 +0400

    add lat and lon into mapContainer

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,37 +1,51 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
 import L, { Icon } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import markerIcon from '../assets/icon-location.svg'
-import styled from 'styled-components';
+import markerIcon from '../assets/icon-location.svg';
+import { useEffect, useRef, useState } from 'react';
 
-function Content() {
-  const position: any = [41.6931, 44.79231];
-    const icon = new Icon({
-      iconUrl: markerIcon,
-      iconSize: [24, 34],
-      iconAnchor: [12, 41],
-    });
-  
-
- 
-  return (
-    <>
-    <MapContainer
-      center={position}
-      zoom={10}
-      style={{ width: '100vw', height: '75vh', zIndex:1 }}
-      
-      >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      <Marker position={position} icon={icon} />
-    </MapContainer>
-      </>
-  );
+interface Props {
+  lat: number;
+  long: number;
 }
 
-export default Content;
+const Content = (props: Props) => {
+  const [center, setCenter] = useState<[number, number]>([
+    props.lat,
+    props.long,
+  ]);
 
+  useEffect(() => {
+    setCenter([props.lat, props.long]);
+  }, [props.lat, props.long]);
+
+  const icon = new Icon({
+    iconUrl: markerIcon,
+    iconSize: [24, 34],
+    iconAnchor: [12, 41],
+  });
+  function ChangeView(props: any) {
+    const map = useMap();
+    map.setView(props.center);
+    return null;
+  }
+
+  return (
+    <>
+      <MapContainer
+        center={center}
+        zoom={14}
+        style={{ width: '100vw', height: '75vh', zIndex: 1 }}
+      >
+        <ChangeView center={[props.lat, props.long]} />
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={center} icon={icon} />
+      </MapContainer>
+    </>
+  );
+};
+
+export default Content;
